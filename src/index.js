5b941acb2b63e3aa96c83e38727fcd5cfa059ccc
Phoebe DG <phoebevdgash@gmail.com>
2018-06-27 14:24:35 +0100

    first two functions complete and cool

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 // Create a function which stores a number. Return an inner function which accepts a number as an argument, adds it to the number in parent scope and returns the new total
 
-function increase(){
-  // implement first exercise here
+function increase() {
+  let number = 5;
+
+  function inner(newNumber) {
+    number = newNumber + number;
+    return number;
+  }
+
+  return inner;
 }
 
-exports.increase = increase;
+function double() {
+  let number = 4;
+
+  function inner() {
+    number = number * 2;
+    return number;
+  }
+
+  return inner;
+}
+
+module.exports = { increase, double };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,18 @@
-const { increase } = require('../src/index.js');
+const { increase, double } = require("../src/index.js");
 
-test('increase', function(){
-  // implement first test here
+test("increase", function() {
+  const arg = 10;
+  const inner = increase();
+  const result = inner(arg);
+
+  const expected = 15;
+
+  expect(result).toBe(expected);
+});
+
+test("double", function() {
+  const inner = double();
+  expect(inner()).toBe(8);
+  expect(inner()).toBe(16);
+  expect(inner()).toBe(32);
 });
